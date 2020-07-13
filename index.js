8ef225b4e8b1cc944f7f08b6e8a11df137b5ceaa
Mike Pokalsky <mpoksky1989@gmail.com>
2020-07-13 15:03:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
-var line = [];
 
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
